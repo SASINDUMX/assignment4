d57c51f17051dadb5b854bc13460e67df0d1f6fa
Sasindu Shalika <MXsasindushalika001@gmail.com>
2024-11-23 23:43:04 +0530

    update animal table

--- assignment4/src/components/AnimalTable.js
+++ assignment4/src/components/AnimalTable.js
@@ -27,26 +27,61 @@ export default function AnimalTable() {
 
   return (
     <div>
-        <h3>{result}<br></br>target animal:{randomAnimal.name}</h3>
+    <table style={{ width: "100%", borderCollapse: "collapse", border: "1px solid black" }}>
+        <thead>
+            <tr>
+                <td style={{ border: "1px solid black", padding: "10px" }}>
+                    <h3>Result</h3>
+                </td>
+                <td style={{ border: "1px solid black", padding: "10px" }}>
+                    <h3>Animal Name</h3>
+                </td>
+                <td style={{ border: "1px solid black", padding: "10px", textAlign: "right" }}>
+                    <h3><center>Select the Animal</center></h3>
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style={{ border: "1px solid black", padding: "10px" }}>
+                    {result}
+                </td>
+                <td style={{ border: "1px solid black", padding: "10px" }}>
+                    {randomAnimal.name}
+                </td>
+                <td style={{ border: "1px solid black", padding: "10px" }}>
+                    <div style={{
+                        display: "grid", 
+                        gridTemplateColumns: "repeat(4, 1fr)", 
+                        gap: "10px"
+                    }}>
+                        {animals.map((animal, index) => (
+                            <button
+                                key={index}
+                                onClick={() => checker(animal.name)}
+                                style={{
+                                    border: "none", 
+                                    background: "none", 
+                                    padding: "0"
+                                }}
+                            >
+                                <img
+                                    src={require(`../assets/pictures/${animal.img}`)}
+                                    alt={animal.name}
+                                    style={{
+                                        width: "100px",
+                                        height: "100px",
+                                        borderRadius: "8px",
+                                    }}
+                                />
+                            </button>
+                        ))}
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
-
-        {animals.map((animal, index) => (
-    <button
-      key={index}
-      onClick={() => checker(animal.name)}
-    >
-      <img
-        src={animal.image}
-        alt={animal.name}
-        style={{
-            width: "100px",
-            height: "100px",
-            borderRadius: "8px",
-          }}
-      />
-    </button>
-))}
-
-    </div>
   );
 }
