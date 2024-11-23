97fc627825b0af844919d44838e7a7b72fdeabcf
Sasindu Shalika <MXsasindushalika001@gmail.com>
2024-11-23 23:37:06 +0530

    update app.js

--- assignment4/src/App.js
+++ assignment4/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AnimalTable from "./components/AnimalTable";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animal Matching Game</h1>
+      <AnimalTable/>
     </div>
   );
 }
